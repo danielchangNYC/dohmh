a84e449ec8e299dd3e8f02ab70cba12d883626eb
Daniel Chang <daniel.chang85@gmail.com>
2016-01-19 23:37:25 -0500

    Convert to symbol headers

--- lib/inspection_results_importer.rb
+++ lib/inspection_results_importer.rb
@@ -17,7 +17,7 @@ class InspectionResultsImporter
   def call
     puts "Reading CSV. This will take a moment..."
 
-    CSV.read(FILE_PATH, headers: true).lazy.each do |row|
+    CSV.read(FILE_PATH, headers: true, header_converters: :symbol).each do |row|
       if valid?(row)
         record_entry!(row)
       else
@@ -45,18 +45,18 @@ class InspectionResultsImporter
   end
 
   def valid?(row)
-    !row['ACTION'].blank? && !row["DBA"].blank?
+    !row[:action].blank? && !row[:dba].blank?
   end
 
   def record_entry!(row)
     begin
-      establishment = Establishment.find_or_initialize_by(camis: row["CAMIS"])
+      establishment = Establishment.find_or_initialize_by(camis: row[:camis])
       update_establishment_from!(establishment, row)
 
       inspection = Inspection.find_or_initialize_by(
         establishment: establishment,
-        inspection_type: row["INSPECTION TYPE"].downcase,
-        inspection_date: DateTime.strptime(row["INSPECTION DATE"], "%m/%d/%Y"))
+        inspection_type: row[:inspection_type].downcase,
+        inspection_date: DateTime.strptime(row[:inspection_date], "%m/%d/%Y"))
 
       update_inspection_from!(inspection, row)
       update_violations!(inspection, row)
@@ -66,20 +66,20 @@ class InspectionResultsImporter
   end
 
   def update_establishment_from!(establishment, row)
-    establishment.dba                 = row["DBA"]
-    establishment.boro                = row["BORO"]
-    establishment.zipcode             = row["ZIPCODE"]
-    establishment.phone               = row["PHONE"] if row["PHONE"]
-    establishment.cuisine_description = row["CUISINE DESCRIPTION"]
+    establishment.dba                 = row[:dba]
+    establishment.boro                = row[:boro]
+    establishment.zipcode             = row[:zipcode]
+    establishment.phone               = row[:phone] if row[:phone]
+    establishment.cuisine_description = row[:cuisine_description]
     establishment.save!
   end
 
   def update_inspection_from!(inspection, row)
-    inspection.action          = row["ACTION"].downcase if row["ACTION"]
-    inspection.score           = row["SCORE"] if row["SCORE"]
-    inspection.grade           = row["GRADE"].downcase if row["GRADE"]
-    inspection.grade_date      = DateTime.strptime(row["GRADE DATE"], "%m/%d/%Y") if row["GRADE DATE"]
-    inspection.record_date     = DateTime.strptime(row["RECORD DATE"], "%m/%d/%Y") if row["RECORD DATE"]
+    inspection.action          = row[:action].downcase if row[:action]
+    inspection.score           = row[:score] if row[:score]
+    inspection.grade           = row[:grade].downcase if row[:grade]
+    inspection.grade_date      = DateTime.strptime(row[:grade_date], "%m/%d/%Y") if row[:grade_date]
+    inspection.record_date     = DateTime.strptime(row[:record_date], "%m/%d/%Y") if row[:record_date]
     inspection.save!
   end
 
@@ -87,10 +87,10 @@ class InspectionResultsImporter
     return false unless violations?(row)
 
     violation = Violation.find_or_initialize_by(
-      code: row["VIOLATION CODE"].downcase,
+      code: row[:violation_code].downcase,
       critical: critical_violation?(row))
 
-    violation.description = row["VIOLATION DESCRIPTION"].downcase if validation.description.blank?
+    violation.description = row[:violation_description].downcase if validation.description.blank?
     violation.save!
 
     if !InspectionViolation.exists? violation: violation, inspection: inspection
@@ -100,10 +100,10 @@ class InspectionResultsImporter
   end
 
   def violations?(row)
-    row["VIOLATION CODE"].present?
+    row[:violation_code].present?
   end
 
   def critical_violation?(row)
-    Violation.critical_flag? row["CRITICAL FLAG"]
+    Violation.critical_flag? row[:critical_flag]
   end
 end
