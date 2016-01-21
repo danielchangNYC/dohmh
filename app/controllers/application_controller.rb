f19d0a41893b942a065f1d0f700c6d146902e98d
Daniel Chang <daniel.chang85@gmail.com>
2016-01-20 22:57:38 -0500

    Display top ten for any type of cuisine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class Dohmh < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :views, Proc.new { File.expand_path('../views', File.dirname(__FILE__)) }
 
-  get '/' do
+  get '/?:cuisine?' do
+    @cuisine = params[:cuisine] || "thai"
+    @establishments = Establishment.top @cuisine
     erb :index
   end
 end
--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -5,10 +5,28 @@ class Establishment < ActiveRecord::Base
   validates :camis, presence: true, uniqueness: true
   validates :dba, presence: true
   validates :boro, presence: true
+  validates :building, presence: true
+  validates :street, presence: true
   validates :zipcode, presence: true
   validates :cuisine_description, presence: true
 
   before_validation :sanitize_fields
+
+  def self.top(cuisine_description, amount=10)
+    includes(:inspections).
+      where("inspections.grade NOT NULL
+        AND cuisine_description like ?", "%#{cuisine_description.downcase}%").
+      order("inspections.grade ASC, inspections.grade_date DESC").
+      take(amount)
+  end
+
+  def display_name
+    dba.split.map(&:capitalize).join(' ')
+  end
+
+  def display_address
+    "#{building} #{street.split.map(&:capitalize).join(' ')}"
+  end
 
   private
 
